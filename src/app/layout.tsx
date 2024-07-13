0ee86c8c757f0ae25ac692d60938c044d94d122a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-07-13 18:30:12 +0700

    Add picocss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Prompt } from "next/font/google";
+import "@picocss/pico";
+import "@picocss/pico/css/pico.colors.min.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const prompt = Prompt({
+  weight: ["400", "700"],
+  subsets: ["latin", "thai"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={prompt.className}>{children}</body>
     </html>
   );
 }
