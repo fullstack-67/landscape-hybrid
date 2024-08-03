d36d6257d3963c99f1436eb47d4feda9b8d28b8d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-04 05:12:13 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,11 @@ interface PageProps {
 
 export default async function Home({ params, searchParams }: PageProps) {
   const todos = await getTodos();
-  // const key = new Date().getTime().toString();
   return (
     <main className="container">
       <a href="/">
         <h1>Todo (RSC + RCC)</h1>
       </a>
-      {/* I set the new key so that the state is reset and I am able to switch between server action. See https://stackoverflow.com/a/77816853*/}
       <FormInput />
       <TodoList todos={todos} />
       <Spinner />
